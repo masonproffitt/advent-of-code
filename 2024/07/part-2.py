bf8d9bc9876573e7878724f40d42fca228f2ac95
Mason Proffitt <mason.proffitt@cern.ch>
2024-12-08 12:55:31 +0100

    add day 7

---
+++ 2024/07/part-2.py
@@ -0,0 +1,40 @@
+import sys
+
+
+add = (lambda x, y: x + y)
+multiply = (lambda x, y: x * y)
+concatenate = (lambda x, y: int(str(x) + str(y)))
+ops = [add, multiply, concatenate]
+
+
+def is_valid(test_value, operands):
+    for op in ops:
+        new_operands = [op(operands[0], operands[1])] + operands[2:]
+        if len(new_operands) == 1:
+            result = new_operands[0] == test_value
+            if result:
+                break
+        else:
+            result = is_valid(test_value, new_operands)
+            if result:
+                break
+    return result
+
+
+if len(sys.argv) > 1:
+    text_source = open(sys.argv[1])
+else:
+    text_source = sys.stdin
+
+lines = text_source.readlines()
+
+total = 0
+for line in lines:
+    test_value_string, rest = line.split(':')
+    test_value = int(test_value_string)
+    operands = [int(x) for x in rest.strip().split(' ')]
+    result = is_valid(test_value, operands)
+    if result:
+        total += test_value
+
+print(total)
